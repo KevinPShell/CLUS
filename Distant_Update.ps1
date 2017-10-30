95532ab18d9cb5357f68111780a188120e0f63e0
Unknown <cordeiro.kevin@gmail.com>
2017-10-30 15:48:38 +0100

    Delete log

--- Distant_Update.ps1
+++ Distant_Update.ps1
@@ -1,10 +1,3 @@
-$logdate = (get-date).tostring('yyyy-MM-dd')
-$log = "C:\WSUSUpdate-$logdate.log"
-
-$now = (get-date).tostring('HH:mm:ss -')
-add-content $log "$now Checking for updates"
-
-
 $computer=Read-Host "Enter server name"
 copy-item -Path "D:\Sources\Scripts\Maintenance\WSUS_v2-AutoReboot.ps1" -Destination (new-item -type directory -force ("\\$computer\d$\Sources\Scripts\Maintenance"+ $newSub)) -force -ea 0
 
